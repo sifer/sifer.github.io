e998e245d2657a497a960a0421f32e732daf0a8e
sifer <max.ekstrom@mensa.se>
2017-02-07 14:00:25 +0100

    hejhej

---
+++ io.bat
@@ -0,0 +1,6 @@
+echo "# sifer.github.io" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/sifer/sifer.github.io.git
+git push -u origin master
